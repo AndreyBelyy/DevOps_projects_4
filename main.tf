8d6dfd34c13928f1b1ac922f1d811aea22f253b3
Andrei Belyi <53856792+AndreyBelyy@users.noreply.github.com>
2023-08-21 17:53:11 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ resource "aws_instance" "arfa" {
     ami             = "ami-053b0d53c279acc90"
     instance_type   = "t2.micro"
     tags = {
-        Name = "jandt"
+        Name = "arfa"
     }
 }
 
